f029bcb83b685a0989f67e9b9873ca3af6201e0e
Poovarasan <153053975+Poovarasan3101@users.noreply.github.com>
2026-09-23 23:40:43 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -117,10 +117,7 @@ REST_FRAMEWORK = {
 # CORS Configuration
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
+   https://poovarasanportfolio.vercel.app/
 ]
 CORS_ALLOW_CREDENTIALS = True
 
