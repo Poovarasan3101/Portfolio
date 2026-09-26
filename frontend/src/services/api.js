fb78b3d44e7fa5ad49337af580e78388a05c2ae5
Poovarasan <153053975+Poovarasan3101@users.noreply.github.com>
2026-09-26 15:21:03 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'https://portfolio-ctjh.onrender.com/api/';
+  'VITE_API_URL=https://poovarasan-portfolio-56v4.onrender.com/api/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
